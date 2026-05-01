c29a2e746e126711af67e2fec57e1891a5c64ad2
JustinStrawberries <Justinl870@nycstudents.net>
2026-05-01 09:55:12 -0400

    apiday2

---
+++ jdwakjdwa.py
@@ -0,0 +1,32 @@
+import requests
+
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+
+for i in range(10):
+    
+
+# pokemon = getPoke("Bulbasaur")
+# print(pokemon)
+
+# def getPoke(poke):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+#     if response.status_code != 200:
+#         print("Error fetching data!")
+#         return None
+    
+
+#     data = response.json()
+#     print(data)
